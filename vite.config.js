c2ef8031d1d53a0cfee41cb4571b46b294a2a793
amanayak <amannayak2911@gmail.com>
2025-06-20 09:31:59 +0530

    new project codingcg

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { VitePluginFonts } from 'vite-plugin-fonts';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePluginFonts({
-      google: {
-        families: ['Poppins:400,600'],
-      },
-    }),
-  ],
+  plugins: [react()],
+  server: {
+    port: 3000,
+  },
 });
