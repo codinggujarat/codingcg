e260488abc66879223c6a9ed151b3f7a52ef3739
amanayak <amannayak2911@gmail.com>
2025-06-20 09:30:02 +0530

    new project codingcg

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
 import { VitePluginFonts } from 'vite-plugin-fonts';
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
     VitePluginFonts({
       google: {
-        families: [
-          {
-            name: 'Poppins',
-            styles: 'wght@400;500;600;700',
-          },
-        ],
+        families: ['Poppins:400,600'],
       },
     }),
   ],
