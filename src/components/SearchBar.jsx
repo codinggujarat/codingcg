b19ed4ed04c006db65d67aff05fa61bea5c4df47
amanayak <amannayak2911@gmail.com>
2025-06-23 09:43:32 +0530

    new project codingcg

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -57,7 +57,7 @@ const SearchBar = () => {
                         />
                         <button
                             onClick={() => handleSearch(query)}
-                            className="bg-black text-white px-4 py-2 text-sm font-semibold"
+                            className="xs:hidden  bg-black text-white px-4 py-2 text-sm font-semibold"
                         >
                             Search
                         </button>
